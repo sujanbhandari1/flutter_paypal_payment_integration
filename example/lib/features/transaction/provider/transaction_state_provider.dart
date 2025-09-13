080af7771a40525c2790b6c3b5b8a8db4b8c43b1
Sujan Bhandari <sujanbhandari@Sujans-MacBook-Pro.local>
2025-09-13 14:53:06 +0545

    documented transaction provider

--- example/lib/features/transaction/provider/transaction_state_provider.dart
+++ example/lib/features/transaction/provider/transaction_state_provider.dart
@@ -1,5 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:paypal_integration/paypal_intregation.dart';
+
+/// This file contains the implementation of the transaction history feature using Riverpod.
+/// It defines a [TransactionHistoryNotifier] class that manages the state of the transaction history,
+/// and a [transactionHistoryProvider] that provides an instance of the notifier to the UI.
 import '../models/transaction_history_model.dart';
 import '../state/transaction_state.dart';
 
@@ -10,6 +14,8 @@ class TransactionHistoryNotifier
 
   final PaypalService paypalService;
 
+  /// Fetches the transaction history from the PayPal API and updates the state.
+  /// It handles loading and error states, and parses the API response into a [TransactionHistoryResponse] object.
   Future<void> fetchTransactions() async {
     state = state.copyWith(isLoading: true, error: null);
 
@@ -44,6 +50,8 @@ class TransactionHistoryNotifier
   }
 }
 
+/// A Riverpod provider that creates and provides an instance of [TransactionHistoryNotifier].
+/// It initializes the [PaypalService] with the necessary credentials and sandbox mode setting.
 final transactionHistoryProvider =
     StateNotifierProvider<TransactionHistoryNotifier, TransactionHistoryState>(
       (ref) => TransactionHistoryNotifier(
